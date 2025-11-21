b16e5f287b293767b35c0b698a050a4ac70344db
Ryan Musser <ryanthomasmusser@gmail.com>
2025-11-21 11:02:58 -0800

    Update DropdownList.tsx
    Try to add origin space back to make pr diff cleaner

--- packages/react/src/components/DataVerification/DropdownList.tsx
+++ packages/react/src/components/DataVerification/DropdownList.tsx
@@ -6,6 +6,7 @@ import {
   mergeBorder,
   setDropcownValue,
 } from "@fortune-sheet/core";
+
 import React, {
   useCallback,
   useContext,
@@ -13,10 +14,12 @@ import React, {
   useRef,
   useState,
 } from "react";
+
 import WorkbookContext from "../../context";
 import { useOutsideClick } from "../../hooks/useOutsideClick";
 import SVGIcon from "../SVGIcon";
 import "./index.css";
+
 const DropDownList: React.FC = () => {
   const { context, setContext } = useContext(WorkbookContext);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -24,12 +27,15 @@ const DropDownList: React.FC = () => {
   const [isMul, setIsMul] = useState<boolean>(false);
   const [position, setPosition] = useState<{ left: number; top: number }>();
   const [selected, setSelected] = useState<any[]>([]);
+  
   const close = useCallback(() => {
     setContext((ctx) => {
       ctx.dataVerificationDropDownList = false;
     });
   }, [setContext]);
+  
   useOutsideClick(containerRef, close, [close]);
+  
   // 初始化
   useEffect(() => {
     if (!context.luckysheet_select_save) return;
@@ -53,6 +59,7 @@ const DropDownList: React.FC = () => {
     const dropdownList = getDropdownList(context, item.value1);
     // 初始化多选的下拉列表
     const cellValue = getCellValue(rowIndex, colIndex, d);
+    
     if (cellValue) {
       setSelected(cellValue.toString().split(","));
     }
@@ -64,6 +71,7 @@ const DropDownList: React.FC = () => {
     setIsMul(item.type2 === "true");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  
   // 设置下拉列表的值
   useEffect(() => {
     if (!context.luckysheet_select_save) return;
@@ -82,8 +90,10 @@ const DropDownList: React.FC = () => {
     if (cellValue) {
       setSelected(cellValue.toString().split(","));
     }
+    
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [context.luckysheetfile]);
+  
   return (
     <div
       id="luckysheet-dataVerification-dropdown-List"
